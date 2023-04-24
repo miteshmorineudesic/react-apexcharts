12e30cadc1565f27f57aa95e8c00b5b8c1ba5c3e
Ashwini-noob-master <119566444+Ashwini-noob-master@users.noreply.github.com>
2023-04-24 13:55:34 +0530

    modified categorybar chart

--- src/Components/ConsumerCategoriesBar.js
+++ src/Components/ConsumerCategoriesBar.js
@@ -26,7 +26,10 @@ const ConsumerCategoriesBar = () => {
             type: 'bar',
             height: 350,
             stacked: true,
-            stackType: '100%',
+            stackType: '3D',
+            toolbar: {
+                show: true
+              },
         },
         responsive: [
             {
@@ -62,11 +65,15 @@ const ConsumerCategoriesBar = () => {
             },
         },
         yaxis:{
+            min: 0,
+                max:100,
+
             axisBorder: {
                 show: 'true',
                 color: '#000000',
-                height: '100%',
+                height: 100,
                 width: 1,
+                
             },
             axisTicks: {
                 show: true,
@@ -80,6 +87,28 @@ const ConsumerCategoriesBar = () => {
               }
 
         },
+        // plotOptions: {
+        //     bar: {
+        //       horizontal: false,
+        //       columnWidth: '70%',
+        //       endingShape: 'flat',
+        //       dataLabels: {
+        //         position: 'top',
+        //       },
+        //     },
+        //   },
+        //   dataLabels: {
+        //     enabled: true,
+        //     formatter: (val, opts) => {
+        //       const cumVal = opts.w.globals.seriesCumulative[opts.seriesIndex][opts.dataPointIndex];
+        //       return cumVal.toFixed(2) + '%';
+        //     },
+        //     offsetY: -20,
+        //     style: {
+        //       fontSize: '12px',
+        //       colors: ['#304758'],
+        //     },
+        //   },
         fill: {
             opacity: 1,
         }, 
@@ -90,9 +119,17 @@ const ConsumerCategoriesBar = () => {
         colors: ['#5b9bd5', '#ed7d31','#a5a5a5','#fec100'], //arrays used to set the color to black and red respectively
        
         tooltip: {
-            followCursor: true,
-          }
-          
+            enabled: true,
+            enabledOnSeries: undefined,
+            shared: true,
+            followCursor: false,
+            intersect: false,
+            inverseOrder: true,
+            style: {
+              fontSize: '12px',
+              fontFamily: undefined,
+            },
+        }
     });
 
     return (
